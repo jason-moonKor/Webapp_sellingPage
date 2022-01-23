b233ebb7b159174bb91e814d59d1a5daf5d8795f
jason-moonKor <jaesangv@naver.com>
2022-01-23 12:42:07 +0900

    version up last

--- product/client/src/main.js
+++ product/client/src/main.js
@@ -1,8 +1,8 @@
 import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
-import mixins from "./mixins.js";
-import store from "./store.js";
+import mixins from "./mixins";
+import store from "./store";
 
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
